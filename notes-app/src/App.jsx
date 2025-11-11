a27f5445e26ee86b357f47b26c3afb2ff95176dc
rokayaothman2003@gmail.com <rokayaothman2003@gmail.com>
2025-11-11 23:53:38 +0200

    Try index as keyy

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -3,19 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const App = (props) => {
-  const {notes} = props
-  const result = notes.map(note => note.content)
+const App = ({ notes }) => {
   return (
-   <ul>
-    {
-      notes.map((note,i) =>
-        <li key={i}>{note.content}</li>
-      )
-    }
-   </ul>
-  );
-  
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <li key={note.id}>
+            {note.content}
+          </li>
+        )}
+      </ul>
+    </div>
+  )
 } 
 
 export default App
