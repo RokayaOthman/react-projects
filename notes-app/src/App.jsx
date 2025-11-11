879db1f51e1cbb729aa2d0272c971d370975108c
rokayaothman2003@gmail.com <rokayaothman2003@gmail.com>
2025-11-11 23:46:50 +0200

    Try index as key

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -7,15 +7,13 @@ const App = (props) => {
   const {notes} = props
   const result = notes.map(note => note.content)
   return (
-    <div>
-      <h1>Notes</h1>
-      <ul> 
-     {result.map((content, index) => (
-          <li key={index}>{content}</li>
-        ))}
-      </ul>
-    </div>
-    
+   <ul>
+    {
+      notes.map((note,i) =>
+        <li key={i}>{note.content}</li>
+      )
+    }
+   </ul>
   );
   
 } 
