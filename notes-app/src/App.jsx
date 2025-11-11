46753be7190a84f6cf185e72a8b5480562b7cf9a
rokayaothman2003@gmail.com <rokayaothman2003@gmail.com>
2025-11-12 01:04:40 +0200

    Move Components folder inside src

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Note from '/./components/Note'
 import './App.css'
 
 const Note = ({note}) => {
