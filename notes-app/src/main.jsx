ef705be02126071bbfdd9bfb6f396d346c1bca9a
rokayaothman2003@gmail.com <rokayaothman2003@gmail.com>
2025-11-11 23:58:06 +0200

    create note module

--- notes-app/src/main.jsx
+++ notes-app/src/main.jsx
@@ -15,7 +15,7 @@ const notes = [
   },
   {
     id: 3,
-    content: 'GETTT and POST are the most important methods of HTTP protocol',
+    content: 'GET and POST are the most important methods of HTTP protocol',
     important: true
   }
 ]
