544be99e2072796fdec9a461de687b269e34e535
rokayaothman2003@gmail.com <rokayaothman2003@gmail.com>
2025-11-11 23:59:28 +0200

    void

--- notes-app/src/main.jsx
+++ notes-app/src/main.jsx
@@ -15,7 +15,7 @@ const notes = [
   },
   {
     id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocols',
+    content: 'GEET and POST are the most important methods of HTTP protocols',
     important: true
   }
 ]
